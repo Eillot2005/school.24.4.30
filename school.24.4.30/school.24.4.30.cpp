0dfa9f35dbf816f276ed0f9b7038be8cdba65f01
lyx <doo8opnewman@outlook.com>
2024-05-06 22:19:45 +0800

    2

--- school.24.4.30/school.24.4.30.cpp
+++ school.24.4.30/school.24.4.30.cpp
@@ -1,13 +1,62 @@
-﻿#include<iostream>
-#include"test.h"
+﻿//#include<iostream>
+//#include"test.h"
+//using namespace std;
+//int main()
+//{
+//	/*int n;
+//	while (cin >> n)
+//	{
+//		T1(n);
+//	}*/
+//
+//	T2();
+//}
+
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//
+//using namespace std;
+//
+//double legendre(int n, double x) {
+//	if (n == 0)
+//		return 1;
+//	else if (n == 1)
+//		return x;
+//	else
+//		return ((2 * n - 1) * x * legendre(n - 1, x) - (n - 1) * legendre(n - 2, x)) / n;
+//}
+//
+//int main() {
+//	double x;
+//	cout << fixed << setprecision(6);
+//
+//	while (cin >> x) {
+//		cout << setw(10) << x << setw(2) << " ";
+//		for (int n = 2; n <= 6; ++n) {
+//			cout << setw(12) << legendre(n, x) << " ";
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
+
+
+
+#include<iostream>
 using namespace std;
+int f[10000009];
 int main()
 {
-	/*int n;
-	while (cin >> n)
+	int i, n;
+	f[1] = 0;
+	f[2] = 0; f[3] = 1;
+	for (i = 4; i <= 10000009; i++)
+		f[i] = f[i / 2] + f[i - i / 2];
+	while (cin>>n)
 	{
-		T1(n);
-	}*/
-
-	T2();
+		cout << f[n] << endl;
+	}
+	return 0;
 }
